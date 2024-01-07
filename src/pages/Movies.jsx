3f3838d227c917a2d1a97c4b1164735d925e6b08
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-01-07 17:25:22 +0200

    try useSearchParams

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+
 const Movies = () => {
+  const { movieId } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+
+  useEffect(() => {
+    //
+  }, []);
+
+  const handleChange = evt => {
+    setSearchParams({ query: evt.currentTarget.value });
+  };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
+        onChange={handleChange}
+        value={query}
         type="text"
-        name="search"
         placeholder="Enter the name of the movie"
       />
       <button type="submit">Search</button>
